60d91f717a6c707ed529ca48a2b72326e3089e79
Michael Maguire <michaelqmaguire2@ufl.edu>
2021-03-31 08:34:37 -0400

    actually corrected indent for consistency

--- 02_import-and-plot.R
+++ 02_import-and-plot.R
@@ -42,29 +42,29 @@ cdc_tp <-
 write_csv(cdc_tp, "./data/clean/cdc-data-cleaned.csv")
 
 cdc_plot <-
-cdc_tp %>%
-  ggplot() +
-    geom_line(aes(x = date_display, y = death_rate, color = drug_type), size = 1.25) +
-    scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
-    scale_y_continuous(expand = c(0.01,0.01), breaks = seq(0, 15, by = 1)) +
-    theme_ipsum_rc(grid = FALSE) +
-    ggtitle("Overdose Death Rates Involving Opioids by Type, United States, 2000 - 2017") +
-    xlab("") +
-    ylab("") +
-    theme(
-      panel.background = element_rect(color = "grey20"),
-      axis.title.x = element_text(angle = 90, color = "black", size = 12),
-      axis.text.x = element_text(color = "black", size = 12),
-      axis.text.y = element_text(color = "black", size = 12),
-      legend.position = "bottom",
-      legend.background = element_rect(linetype = "solid"),
-      legend.text = element_text(size = 12)
-      ) +
-    scale_color_manual(
-      name = "",
-      labels = c("Any Opioid", "Common Opioids (e.g. Fentanyl, Tramadol)", "Heroin", "Synthetic Opioids (Natural & Semi-Synthetic Opioids and Methadone)"),
-      values = c("tan2", "dodgerblue2", "forestgreen", "grey20")
-    )
+  cdc_tp %>%
+    ggplot() +
+      geom_line(aes(x = date_display, y = death_rate, color = drug_type), size = 1.25) +
+      scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+      scale_y_continuous(expand = c(0.01,0.01), breaks = seq(0, 15, by = 1)) +
+      theme_ipsum_rc(grid = FALSE) +
+      ggtitle("Overdose Death Rates Involving Opioids by Type, United States, 2000 - 2017") +
+      xlab("") +
+      ylab("") +
+      theme(
+        panel.background = element_rect(color = "grey20"),
+        axis.title.x = element_text(angle = 90, color = "black", size = 12),
+        axis.text.x = element_text(color = "black", size = 12),
+        axis.text.y = element_text(color = "black", size = 12),
+        legend.position = "bottom",
+        legend.background = element_rect(linetype = "solid"),
+        legend.text = element_text(size = 12)
+        ) +
+      scale_color_manual(
+        name = "",
+        labels = c("Any Opioid", "Common Opioids (e.g. Fentanyl, Tramadol)", "Heroin", "Synthetic Opioids (Natural & Semi-Synthetic Opioids and Methadone)"),
+        values = c("tan2", "dodgerblue2", "forestgreen", "grey20")
+      )
 
 png("./plots/cdc-opioid-death-rate-figure.png", res = 1200, height = 8, width = 12, units = "in")
 cdc_plot
